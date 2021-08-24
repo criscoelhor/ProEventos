140a9db084a41358e67ce221ce1eccb49963c890
criscoelhor <coelhocr@gmail.com>
2021-08-23 22:15:51 -0300

    Interpolation, Directive and Binding

--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -8,7 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
-  public eventos: any;
+  public events: any = [];
+  public filteredEvents: any = [];
+  widthImg = 100;
+  marginImg = 2;
+  showImg = true;
+  private _filter: string = '';
+
+  public get filter(){
+    return this._filter;
+  }
+
+  public set filter(value){
+    this._filter = value;
+    this.filteredEvents = this.filter ? this.filterEvents(this.filter) : this.events;
+  }
+
+  filterEvents(filterBy: string): any {
+    filterBy = filterBy.toLocaleLowerCase();
+    return this.events.filter(
+      (evento : any) => evento.tema.toLocaleLowerCase().indexOf(filterBy) !== -1
+    )
+  }
 
   constructor(private http: HttpClient) { }
 
@@ -16,9 +37,16 @@ export class EventosComponent implements OnInit {
     this.getEventos();
   }
 
+  showImage(){
+    this.showImg = !this.showImg;
+  }
+
   public getEventos(): void{
     this.http.get('https://localhost:5001/api/eventos').subscribe(
-      response => this.eventos = response,
+      response => {
+        this.events = response,
+        this.filteredEvents = this.events
+      },
       error => console.log(error)
     );
   }
